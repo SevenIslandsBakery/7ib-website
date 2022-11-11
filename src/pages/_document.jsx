d29bc0efc3eb918348e1f3560d2725cb4b86036b
Yash Goyal <yg@Yashs-MacBook-Air.local>
2022-11-11 19:27:24 +0530

    added google analytics

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,7 +1,6 @@
 import { Head, Html, Main, NextScript } from 'next/document'
 import Script from 'next/script'
 
-
 export default function Document(props) {
   let pageProps = props.__NEXT_DATA__?.props?.pageProps
 
@@ -11,6 +10,19 @@ export default function Document(props) {
       lang="en"
     >
       <Head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-9J0S5175FC"
+        ></Script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-9J0S5175FC');`,
+          }}
+        ></script>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
